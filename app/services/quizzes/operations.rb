1911a5ac7e58b24931790640d4eb52656516bb08
Savon Williams <savon817@live.missouristate.edu>
2023-04-06 20:25:27 -0500

    Update: Working on fixing Update Quiz

--- app/services/quizzes/operations.rb
+++ app/services/quizzes/operations.rb
@@ -2,7 +2,6 @@ module Quizzes
     module Operations
         def self.new_quiz(params, current_user)
             questions = []
-            debugger
             params[:questions].each do |question|
                 hash = {question: question[:question], answer: question[:answer], possible_answers: question[:possible_answers]}
                 questions.push(hash)
@@ -17,8 +16,15 @@ module Quizzes
 
         def self.update_quiz(params)
             quiz = Quiz.find(params[:id])
+            questions = params[:questions].map do |question|
+                {
+                  question: question[:question],
+                  answer: question[:answer],
+                  possible_answers: question[:possible_answers]
+                }
+              end
 
-            return ServiceContract.success(quiz) if quiz.update(title: params[:title], description: params[:description], questions: params[:questions])
+            return ServiceContract.success(quiz) if quiz.update(title: params[:title], description: params[:description], questions: question)
 
             ServiceContract.error(quiz.error.full_messages)
         end
