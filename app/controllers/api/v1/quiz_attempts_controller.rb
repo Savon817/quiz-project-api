f98f37b4a727ff394e38f713c43f26d666c4001a
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-04-19 18:23:05 -0500

    Update: working on scoring quiz

--- app/controllers/api/v1/quiz_attempts_controller.rb
+++ app/controllers/api/v1/quiz_attempts_controller.rb
@@ -1,7 +1,7 @@
 module Api
     module V1
             class QuizAttemptsController < ApplicationController
-                before_action :authenticate
+                 before_action :authenticate
                 before_action :set_quiz, only: [:create]
                 
 
@@ -9,16 +9,16 @@ module Api
                 def create
                     quiz_attempt = @quiz.quiz_attempts.build(quiz_attempt_params)
                     quiz_attempt.user = @current_user
-                    debugger
                     if quiz_attempt.save
-                      # Grade the quiz attempt
+
+                      # Grade the quiz attempt and update completed status
                       score = 0
                       quiz_attempt.answers.each_with_index do |answer, index|
-                        if answer == @quiz.possible_answers[index]
+                        if answer == @quiz.questions[index][:answer]
                           score += 1
                         end
                       end
-                      quiz_attempt.update(score: score)
+                      quiz_attempt.update(score: score, completed: true)
                 
                       render json: quiz_attempt, status: :created
                     else
