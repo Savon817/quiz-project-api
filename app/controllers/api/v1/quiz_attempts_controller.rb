494dc6cb1298005f60e126db1c7cd30ebfb2d4b3
Savon Williams <44207109+Savon817@users.noreply.github.com>
2023-04-20 09:13:57 -0500

    Update: Added Comments explaining what QuizAttemptController does

--- app/controllers/api/v1/quiz_attempts_controller.rb
+++ app/controllers/api/v1/quiz_attempts_controller.rb
@@ -1,21 +1,27 @@
 module Api
     module V1
             class QuizAttemptsController < ApplicationController
-                 before_action :authenticate
+                # Authenicating the user before the Attempted Quiz is submitted
+                before_action :authenticate
+                
+                # Making sure a quiz exist before quizAttempt executes
                 before_action :set_quiz, only: [:create]
                 
 
-                # POST api/v1/quizzes/21/quiz_attempts or at least that's it's supposed to do
+                # POST api/v1/quizzes/1/quiz_attempts
                 def create
+                    # Creates a new quiz attempt and then links it to the user
                     quiz_attempt = @quiz.quiz_attempts.build(quiz_attempt_params)
                     quiz_attempt.user = @current_user
+                    
                     if quiz_attempt.save
-
                       # Grade the quiz attempt and update completed status
                       score = 0
                       quiz_attempt.answers.each_with_index do |answer, index|
+                        # Grabs User selected answer and the correct answer from each question in the quiz
                         selected_answer = answer[1]
                         correct_answer = @quiz.questions[index][:answer]
+                        # Compares Selected and Correct answers, then increments the score if they match
                         if selected_answer == correct_answer
                             score += 1
                         end
@@ -28,6 +34,7 @@ module Api
                     end
                   end
                 
+                # GET api/v1/quiz_attempts/1
                 def show
                     quiz_attempt = @current_user.quiz_attempts.find(params[:id])
                     
